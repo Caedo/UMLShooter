bd672e4dcac08ec0862e2e139ab41311e92d4f8a
Paweł Dziedzic <blavikenn@gmail.com>
2019-03-26 16:34:29 +0100

    Add simple player placeholder and movement scripts

---
+++ UMLShooter/Assets/_Scripts/PlayerInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+    Transform _cameraTransform;
+
+    PlayerMovement _movement;
+
+    private void Awake() {
+        _cameraTransform = Camera.main.transform;
+        _movement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update() {
+        var forward = _cameraTransform.forward;
+        forward.y = 0;
+
+        var right = _cameraTransform.right;
+
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        var velocity = forward.normalized * v + right.normalized * h;
+        _movement.Move(velocity);
+    }
+
+}
---
+++ UMLShooter/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+
+    public float _Speed;
+    Rigidbody _body;
+
+    void Awake() {
+        _body = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 velocity) {
+        _body.MovePosition(_body.position + velocity * Time.deltaTime * _Speed);
+    }
+}

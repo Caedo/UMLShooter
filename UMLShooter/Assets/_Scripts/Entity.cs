503f672122600a60ba6a44094725680cef2c3258
Paweł Dziedzic <blavikenn@gmail.com>
2019-03-26 16:34:53 +0100

    Add simplme health scripts

---
+++ UMLShooter/Assets/_Scripts/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour {
+    public float _Health;
+
+    protected virtual void TakeDamage(float dmg) {
+        _Health -= dmg;
+        if (_Health <= 0) {
+            Death();
+        }
+    }
+
+    protected virtual void Death() {
+
+    }
+}
---
+++ UMLShooter/Assets/_Scripts/PlayerEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEntity : Entity
+{
+    public System.Action OnPlayerDeath;
+
+    protected override void Death()
+    {
+        OnPlayerDeath?.Invoke();
+        Destroy(gameObject);
+    }
+}

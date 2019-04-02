92f52c7f55655d16fe8c590d86e906922e61683a
Paweł Dziedzic <blavikenn@gmail.com>
2019-04-02 13:32:18 +0200

    Add simple camera follow script

---
+++ UMLShooter/Assets/_Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform _Target;
+    public bool _UseStartOffset;
+    public Vector3 _Offset;
+
+    Vector3 _CurrentOffset;
+
+    void Start() {
+        _CurrentOffset = _UseStartOffset ? transform.position - _Target.position : _Offset;
+    }
+
+    private void LateUpdate() {
+        transform.position = _Target.position + _CurrentOffset;
+    }
+}

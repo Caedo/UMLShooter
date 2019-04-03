184afee1e7c7d6c443072e16b608a133b8dc916f
Paweł Dziedzic <blavikenn@gmail.com>
2019-04-03 21:50:20 +0200

    Add Weapon abstract class, remove WeaponData
    Changed conception of storing weapon data, instead of Scriptable object they will be store in actual Weapon class

---
+++ UMLShooter/Assets/_Scripts/Shooting/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+    public float _damage;
+    public int _maxAmmo;
+    public int _currentAmmo;
+
+    public bool _infinityAmmo;
+    public bool _avaible;
+    
+    public abstract void Fire();
+}

e1e698a6d7e87cc18e14a413cf580da6aa276f80
Paweł Dziedzic <blavikenn@gmail.com>
2019-04-04 20:12:54 +0200

    Add first enemy scripts

---
+++ UMLShooter/Assets/_Scripts/Enemy/EnemyTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTest : MonoBehaviour {
+
+    public float _moveSpeed;
+    public float _attackTreshold;
+    PlayerEntity _player;
+
+    void Awake() {
+        _player = GameObject.FindObjectOfType<PlayerEntity>();
+    }
+
+    void Update() {
+        var dir = transform.position - _player.transform.position;
+        transform.Translate(dir.normalized * _moveSpeed * Time.deltaTime);
+
+        if (dir.sqrMagnitude < _attackTreshold) {
+            _player.TakeDamage(15f);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ UMLShooter/Assets/_Scripts/EnemySpawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Transform[] _spawnPoints;
+    public float _timeBetweenSpawns;
+
+    void Start()
+    {
+        
+    }
+
+}

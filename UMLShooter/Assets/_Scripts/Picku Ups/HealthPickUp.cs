b4c8a3b3830c5c4702f3ed6102614c87485fc43a
Paweł Dziedzic <blavikenn@gmail.com>
2019-04-04 20:13:26 +0200

    Add some pickup scripts

---
+++ UMLShooter/Assets/_Scripts/Picku Ups/HealthPickUp.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickUp : PickUp {
+    public float _healAmount;
+
+    public override void Affect(GameObject Player) {
+        Player.GetComponent<PlayerEntity>().TakeDamage(-_healAmount);
+    }
+}
---
+++ UMLShooter/Assets/_Scripts/Picku Ups/PickUp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PickUp : MonoBehaviour {
+
+    public abstract void Affect(GameObject Player);
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            Affect(other.gameObject);
+        }
+    }
+}

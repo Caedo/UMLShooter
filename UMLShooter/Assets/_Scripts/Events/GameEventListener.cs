b9344e683a41988b140b51b60d9d068f9fff9edc
Paweł Dziedzic <blavikenn@gmail.com>
2019-04-04 20:11:58 +0200

    Add game events scripts

---
+++ UMLShooter/Assets/_Scripts/Events/GameEventListener.cs
@@ -0,0 +1,37 @@
+﻿// ----------------------------------------------------------------------------
+// Unite 2017 - Game Architecture with Scriptable Objects
+// 
+// Author: Ryan Hipple
+// Date:   10/04/17
+// ----------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace RoboRyanTron.Events {
+    public class GameEventListener : MonoBehaviour {
+        [Tooltip("Events list to register with.")]
+        public List<GameEvent> Events;
+
+        [Tooltip("Response to invoke when Event is raised.")]
+        public UnityEvent Response;
+
+        private void OnEnable() {
+            foreach (var item in Events) {
+                item.RegisterListener(this);
+
+            }
+        }
+
+        private void OnDisable() {
+            foreach (var item in Events) {
+                item.UnregisterListener(this);
+            }
+        }
+
+        public void OnEventRaised() {
+            Response.Invoke();
+        }
+    }
+}

34dbac45d75c1d84b1f46eb4ff585f16cf010d8b
Paweł Dziedzic <blavikenn@gmail.com>
2019-04-06 20:06:12 +0200

    Add raycast weapon

---
+++ UMLShooter/Assets/_Scripts/Shooting/RaycastWeapon.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastWeapon : Weapon {
+    public float _roundsPerSecond;
+    public LineRenderer _line;
+
+    float _shootTimer;
+    float _timeBetweenBullets;
+
+    void Start() {
+        _timeBetweenBullets = 1 / _roundsPerSecond;
+    }
+
+    private void Update() {
+        _shootTimer += Time.deltaTime;
+    }
+
+    public override void Fire() {
+        if (_shootTimer > _timeBetweenBullets && (_currentAmmo > 0 || _infinityAmmo)) {
+
+            RaycastHit hit;
+            if (Physics.Raycast(_gunPoint.position, _gunPoint.forward, out hit)) {
+                var entity = hit.collider.GetComponentInParent<Entity>();
+                if (entity) {
+                    entity.TakeDamage(_damage);
+                }
+
+                _line.SetPosition(0, _gunPoint.position);
+                _line.SetPosition(1, hit.point);
+
+                StartCoroutine(LineRoutine());
+            }
+
+            _shootTimer = 0;
+        }
+    }
+
+    IEnumerator LineRoutine(){
+        _line.enabled = true;
+        yield return new WaitForSeconds(_timeBetweenBullets / 2);
+        _line.enabled = false;
+    }
+
+}
